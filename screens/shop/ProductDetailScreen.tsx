df926f85d1958474109fdf0beecacc1ad2530f41
Simas <simas@simas-dei.com>
2020-02-01 17:03:02 +0200

    directory setup 1/2

---
+++ screens/shop/ProductDetailScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ProductDetailScreen = () => {
+  return (
+    <View>
+      <Text>Product details</Text>
+    </View>
+  );
+};
+
+export default ProductDetailScreen;
---
+++ screens/shop/ProductsOverviewScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ProductsOverviewScreen = () => {
+  return (
+    <View>
+      <Text>Product screen</Text>
+    </View>
+  );
+};
+
+export default ProductsOverviewScreen;
