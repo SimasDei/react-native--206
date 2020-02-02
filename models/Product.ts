85f26ad2c48d67841ea1d2ea3a0e9ddb1d61191e
Simas <simas@simas-dei.com>
2020-02-02 16:33:42 +0200

    add Product Model

---
+++ models/Product.ts
@@ -0,0 +1,26 @@
+import { IProduct } from '../types';
+
+class Product implements IProduct {
+  id: IProduct['id'];
+  ownerId: IProduct['ownerId'];
+  title: IProduct['title'];
+  imageUrl: IProduct['imageUrl'];
+  description: IProduct['description'];
+  price: IProduct['price'];
+
+  constructor(
+    id: Product['id'],
+    ownerId: IProduct['ownerId'],
+    title: IProduct['title'],
+    imageUrl: IProduct['imageUrl'],
+    description: IProduct['description'],
+    price: IProduct['price'],
+  ) {
+    this.id = id;
+    this.ownerId = ownerId;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+}
