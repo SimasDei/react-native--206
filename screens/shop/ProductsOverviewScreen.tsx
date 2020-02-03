0f800e54ecb151296d590db9a0c26cc9d6c33135
Simas <simas@simas-dei.com>
2020-02-03 17:39:57 +0200

    add placeholder styles for ProductsOverview Screen

--- screens/shop/ProductsOverviewScreen.tsx
+++ screens/shop/ProductsOverviewScreen.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
 
 import { IRootState } from '../../types';
+import { COLORS } from '../../constants';
 
 export const ProductsOverviewScreen = () => {
   const products = useSelector((state: IRootState) => state.products.availableProducts);
 
-  return <FlatList data={products} renderItem={({ item }) => <Text>{item.title}</Text>} />;
+  return (
+    <FlatList
+      style={styles.container}
+      data={products}
+      renderItem={({ item }) => <Text style={styles.productText}>{item.title}</Text>}
+    />
+  );
 };
+
+ProductsOverviewScreen.navigationOptions = {
+  headerTitle: 'All Products',
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.primaryDark,
+  },
+  productText: {
+    color: '#fff',
+  },
+});
 
 export default ProductsOverviewScreen;
